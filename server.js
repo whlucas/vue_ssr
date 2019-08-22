a8e63d8a1d26b616bfc2dddc269614ccf4b005e7
jialin <haolin.whl@alibaba-inc.com>
2019-08-22 13:19:41 +0800

    注释

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-const express = require('express')
 const http = require('http')
-// const server = express()
 const serverRenderer = require('vue-server-renderer')
 
 // 拿到打包好的vue实例
 const createApp = require('./dist/bundle.server.js')['default']
 
+// 拿到模板
 const renderer = serverRenderer.createRenderer({
     template: require('fs').readFileSync('./index.html','utf8')
 })
 
+// 吐回给前端
 const server = http.createServer((req, res) => {
     let app = createApp()
     renderer.renderToString(app, (err, html) => {
