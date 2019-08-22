9a88eeb003bfe67333f8ff1a04bd018e052425d0
whlucas <1213372700@qq.com>
2019-08-22 23:00:54 +0800

    加注释

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
  const http = require('http')
  const Vue = require('vue')
  const ServerRenderer = require('vue-server-renderer')
+
+ // 生成一个html实例
  const app = new Vue({
     template: `<div>
                     <input v-model='val' />
@@ -11,15 +13,19 @@
     }
  })
 
- // 用这个包的toString方法把他转到
+ // 实例化一个renderer对象，里面有将vue对象转化成字符串的方法
  const renderer = ServerRenderer.createRenderer({
     //  这里创建的时候可以给他一个模板，在模板里面做一个标记，它就知道放哪了
      template: require('fs').readFileSync('./index.html','utf8')
  })
 
+
+ 
  const server = http.createServer((req, res) => {
+
+    // 将这个app实例转化成字符串，最后吐到客户端
      renderer.renderToString(app, {
-         init: '<script> var a = 10; console.log; </script>'
+         init: '<script> var a = 10; console.log(a); </script>'
         }, (err, html) => {
         if(err){
             console.log(err)
@@ -28,4 +34,5 @@
         }
      })
  })
+
  server.listen(8090)
